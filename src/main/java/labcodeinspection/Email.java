2c84d56ddaab62d9ff71b95f0a736afc56c8fa58
unknown <ricluper@espol.edu.ec>
2024-06-06 21:31:29 -0500

    'code'

---
+++ src/main/java/labcodeinspection/Email.java
@@ -0,0 +1,67 @@
+package labcodeinspection;
+
+import java.util.Locale;
+
+public class Email {
+
+	private String m_firstName;
+	private String m_lastName;
+	private String password;
+	private String department;
+	private int defaultpasswordLength = 8;
+	private String email;
+	
+	/**
+	 * Constructor for Email class.
+	 */
+	public Email(String firstName, String lastName) {
+		this.m_firstName = firstName;
+		this.m_lastName = lastName;
+	}
+
+	/**
+	 * Method to show info.
+	 */
+	public void showInfo() {
+		System.out.println("\nFIRST NAME= " + m_firstName + "\nLAST NAME= " + m_lastName);
+		System.out.println("DEPARMENT= " + department + "\nEMAIL= " + email + "\nPASSWORD= " + password);
+	}
+
+	/**
+	 * Method to show info.
+	 */
+	public void setDeparment(int depChoice) {
+		switch (depChoice) {
+		case 1:
+			this.department = "sales";
+			break;
+		case 2:
+			this.department = "dev";
+			break;
+		case 3:
+			this.department = "acct";
+			break;
+		default:
+			this.department = "unknown";
+		}
+	}
+
+	private String randomPassword(int length) {
+		String set = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890#$&@*";
+		char[] password = new char[length];
+		for (int i = 0; i < length; i++) {
+			int rand = (int) (Math.random() * set.length());
+			password[i] = set.charAt(rand);
+		}
+		return new String(password);
+	}
+
+	/**
+	 * Method to generate an email.
+	 */
+	public void generateEmail() {
+		this.password = this.randomPassword(this.defaultpasswordLength);
+		this.email = this.m_firstName.toLowerCase(Locale.US) + this.m_lastName.toLowerCase(Locale.US) + "@" + this.department
+				+ ".espol.edu.ec";
+	}
+}
